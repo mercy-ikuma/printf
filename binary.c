9c8ba8bb694d56dd021aad234984940992bde408
mercy-ikuma <84702006+mercy-ikuma@users.noreply.github.com>
2021-07-26 23:09:39 +0300

    print binary

--- binary.c
+++ binary.c
@@ -1,40 +1,21 @@
 #include "holberton.h"
 
 /**
- * print_binary - print unsigned int as a binary
- * @modif: struct containing modifier fields
- * @ap: va_list pointer containing unsigned int to convert and print
  *
- * Return: number of printed charachters
  */
 
-char *print_binary(modifier_t *modif, va_list ap)
+int print_binary(char *format_spec, va_list ap)
 {
-	unsigned int n;
-	int i = 0, j = 0;
-	char binary[35], *res_str;
+	unsigned int count = 0, n;
 
-	if (!ap || !modif || modif->specifier != 'b')
+	if (!ap || !format_spec)
 		return (0);
 	n = va_arg(ap, unsigned int);
-	if (n == 0)
+	while (n)
 	{
-		j = 1;
-		res_str = malloc(sizeof(char) * 2);
-		res_str[0] = '0';
+		_putchar((n % 2) + '0');
+		n = n / 2;
+		count++;
 	}
-	else
-	{
-		while (n)
-		{
-			binary[i++] = '0' + (n % 2);
-			n = n / 2;
-		}
-		res_str = malloc(sizeof(char) * i);
-		i--;
-		while (i >= 0)
-			res_str[j++] = binary[i--];
-	}
-	res_str[j] = '\0';
-	return (res_str);
+	return (count);
 }
