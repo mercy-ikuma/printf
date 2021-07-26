62ceb4ccb208636a96f2d81980eb333e40c9c651
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-07-26 14:29:03 +0200

    Added a file with printf initializer

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * _printf - This function implement a printf function of C
+ * allows to format string according your format specifier
+ * @format: This is the input to print in standard output
+ *
+ * Return: length  of output string printed
+ */
+int _printf(const char *format, ...)
+{
+	/* char buffer[SIZE]; */
+	/* const char *posc_act, *posc_next; */
+	int (*p)();
+	int len_print = 0;
+	int i;
+	va_list list;
+
+	if ((!format) || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+	va_start(list, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			p = select_fun_fmt(format[i + 1]);
+			if (p)
+			{
+				len_print += p(list);
+			} else
+			{
+				if (format[i + 1] == '%')
+					len_print += _putchar(format[i + 1]);
+				else
+				{
+					len_print += _putchar(format[i]);
+					len_print += _putchar(format[i + 1]);
+				}
+			}
+			i++;
+		} else
+		{
+			_putchar(format[i]);
+			len_print++;
+		}
+	}
+	va_end(list);
+	return (len_print);
+}
